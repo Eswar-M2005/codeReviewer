110b351738c996087f21bfe736a38364e63d499a
ESWAR.MANDAPATI <eswar.mandapati@ESWARMANDAPATIs-MacBook-Pro.local>
2026-02-13 21:39:08 +0530

    testing hook

--- sample_py.py
+++ sample_py.py
@@ -1,1 +1,13 @@
-x = 10
+def add(a, b):
+    x = 10
+    y = a + b
+    return y
+
+def process_data(a, b, c, d, e,f):
+    try:
+        total = 0
+        for i in range(5):
+            total += i
+        print(total)
+    except:
+        pass
